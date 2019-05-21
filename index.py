fd9f1f76bcdc088e1fc33a2e400063f5d6a0e45c
Jon McClure <jmcclure@politico.com>
2019-05-21 14:05:05 -0400

    log event body

--- index.py
+++ index.py
@@ -3,6 +3,6 @@ from server import app
 
 
 def handler(event, context):
-    print("EVENT", event)
+    print("EVENT", event.get("body", "NO BODY"))
     print("CONTEXT", context)
     return awsgi.response(app, event, context)
